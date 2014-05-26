b77f9f9555d0d25f90d9d0e742a072b902721445
Miao Sun <miaosun88@gmail.com>
2014-05-26 15:14:14 +0100

    Profile, Order, myOrders Pages access permission fix

--- proto/pages/users/myOrders.php
+++ proto/pages/users/myOrders.php
@@ -4,9 +4,15 @@ include_once($BASE_DIR .'database/users.php');
 include_once($BASE_DIR .'database/orders.php');
 
 $user = getBuyerByEmail($_SESSION['email']);
-$smarty->assign('user', $user);
-
 $myOrders = getOrdersByBuyer($user['iduser']);
+
+if ($_SESSION['permission'] != 1 && $_SESSION['permission'] != 2) {
+    if ($_SESSION['email'] == null) {
+        header('Location: ' . $NO_ACCESS);
+    }
+}
+
+$smarty->assign('user', $user);
 $smarty->assign('myOrders', $myOrders);
 
 $smarty->display('users/myOrders.tpl');
--- proto/pages/users/order.php
+++ proto/pages/users/order.php
@@ -6,14 +6,24 @@ include_once($BASE_DIR .'database/orders.php');
 //$orderId = filter_input(INPUT_GET, 'id');
 $orderId = $_GET['id'];
 
+$userid = filter_input(INPUT_GET, 'id');
+
+$orderDetail = getOrderDetailById($orderId);
+$orderTotal = getOrderTotal($orderId);
+$orderLines = getOrderLinesById($orderId);
+
+
+if ($_SESSION['permission'] != 1 && $_SESSION['permission'] != 2) {
+    if ($_SESSION['email'] == null || $_SESSION['email'] != $orderDetail['email']) {
+        header('Location: ' . $NO_ACCESS);
+    }
+}
+
 if (!isset($orderId)) {
     print_r("Invalid Request params.");
     exit();
 }
 
-$orderDetail = getOrderDetailById($orderId);
-$orderTotal = getOrderTotal($orderId);
-$orderLines = getOrderLinesById($orderId);
 
 $smarty->assign('orderDetail', $orderDetail);
 $smarty->assign('orderTotal', $orderTotal);
